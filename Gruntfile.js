1e40e818d71277ca7b0bfb65f2faaeec529cb31b
Tom Hewitt <tom@colourflood.co.uk>
2014-10-22 18:10:53 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,97 @@
+module.exports = function(grunt) {
+
+	// LOAD TASKS FROM PACKAGE MANEFEST
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+	// GRUNT CONFIG
+	grunt.initConfig({
+
+		// IMPORT MANIFEST
+		pkg: grunt.file.readJSON('package.json'),
+
+	    // CONCAT
+		concat: {
+			dist: {
+				src: [
+					'libs/src/js/script/*.js'],
+				dest: 'libs/src/js/bundle.js'
+			}
+		},
+
+		// MINIFY
+		uglify: {
+			options: {
+				compress: {
+			    	drop_console: true
+			    }
+			},
+			target: {
+				src: ['libs/src/js/bundle.js'],
+				dest: 'libs/dist/js/bundle.min.js'
+			}
+		},
+
+		// COMPASS
+		compass: {
+			dist: {
+				options: {
+					sassDir: 'libs/src/scss/',
+					cssDir: 'libs/src/css/',
+					environment: 'production',
+					outputStyle: 'compressed'
+				}
+			}
+		},
+
+		// AUTOPREFIX
+		autoprefixer: {
+			options: {
+				formatting : {
+					indent_size : 4
+				}
+			},
+			files: {
+				src: 'libs/src/css/bundle-noprefix.css',
+				dest: 'libs/dist/css/bundle.css'
+			}
+		},
+
+		// WATCH
+		watch: {
+			all: {
+				files: 'index.html',
+				options: {
+					livereload: true
+				}
+			},
+			scripts: {
+				files: 'libs/src/js/script/*.js',
+				tasks: ['concat', 'uglify'],
+				options: {
+					livereload: true
+				}
+			},
+			scss: {
+				files: 'libs/src/scss/*.scss',
+				tasks: ['compass'],
+				options: {
+					livereload: true
+				}
+			},
+			css: {
+				files: 'libs/src/css/bundle-noprefix.css',
+				tasks: ['autoprefixer'],
+				options: {
+					livereload: true
+				}
+			},
+		}
+	});
+
+	// CREATE TASK 'default'
+	grunt.registerTask('default', ['concat', 'uglify', 'compass', 'autoprefixer']);
+
+};
+
+
+
---
+++ libs/src/js/script/init.js
@@ -0,0 +1,108 @@
+// *****************************************************************
+// TIMER MODULE
+// *****************************************************************
+Timer = (function($) {
+	
+	var counting;
+	var hours = 0;
+	var minutes = 0;
+	var seconds = 0;
+	var miliseconds = 0;
+
+	// constructor
+	var Timer = function() {
+
+	}
+
+	// private
+
+	function update() {
+		miliseconds++;
+		if (miliseconds >= 100) {
+			miliseconds = 0;
+			seconds++;
+		    if (seconds >= 60) {
+		        seconds = 0;
+		        minutes++;
+		        if (minutes >= 60) {
+		            minutes = 0;
+		            hours++;
+		        }
+		    }
+		}    
+	    	
+	    updateTimer();
+	}
+
+	function updateTimer() {
+		$('.timer span').text((hours ? (hours > 9 ? hours : '0' + hours) : '00') + ':' + 
+	    	(minutes ? (minutes > 9 ? minutes : '0' + minutes) : '00') + ':' + 
+	    	(seconds ? (seconds > 9 ? seconds : '0' + seconds) : '00') + ':' +
+	    	(miliseconds > 9 ? miliseconds : '0' + miliseconds));
+	}
+
+	// public
+	Timer.start = function() {
+		counting = setInterval(function(){update()}, 10);
+	}
+
+	Timer.stop = function() {
+		clearInterval(counting);
+	}
+
+	Timer.reset = function() {
+		hours = 0;
+		minutes = 0;
+		seconds = 0;
+		miliseconds = 0;
+
+		updateTimer();
+	}
+
+	Timer.lap = function() {
+		var laptime = $('.timer span').text();
+		var lapint = $('.laps div').length + 1;
+		$('.laps').append('<div class="laptime"><span>' + lapint + '</span><span>' + laptime + '</span></div>');
+	}
+
+	return Timer;
+
+})(jQuery);
+
+
+// *****************************************************************
+// INIT
+// *****************************************************************
+$(document).ready(function() {
+	
+	// setup flowtype	
+	$('.timer').flowtype();
+
+	// start/stop timer
+	$('.timer-controls .startstop').click(function() {
+		if ($(this).hasClass('started')) {
+			Timer.stop();
+			$(this).removeClass('started');
+		} else {
+			Timer.start();
+			$(this).addClass('started');
+		}
+	});
+
+	// reset
+	$('.timer-controls .reset').click(function() {
+		Timer.reset();
+	});
+	
+	// lap
+	$('.timer-controls .lap').click(function() {
+		Timer.lap();
+	});
+
+	// clear
+	$('.timer-controls .clear').click(function() {
+		$('.laps').html('');
+	});
+
+});
+
