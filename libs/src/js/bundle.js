beabccd3efff4d51cccd4b580a361b763bdc6a24
Tom Hewitt <tom@colourflood.co.uk>
2014-10-27 00:03:21 +0000

    final prototype

--- libs/src/js/bundle.js
+++ libs/src/js/bundle.js
@@ -8,9 +8,14 @@ var Lipsum = (function() {
 
 	// private
 	var defaultText = {
-		normal: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+		normal: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 
 		wars: 'Lucas...'
+
+		//****************************/
+		// TO DO
+		// make lucas text
+		//****************************/
 	}
 
 	var params = [
@@ -21,48 +26,114 @@ var Lipsum = (function() {
 	];
 
 	function generate() {
-		var textarea = document.querySelector('.lipsum-text');
-		var textArr = defaultText['normal'].split(' ');
+		var $textarea = document.querySelector('.lipsum-text');
 		var newTextWords = '';
 		var newTextParas = '';
+		var textArr;
+		var i;
+		var j;
+
+		// setup text mode
+		if (params[3].val == 0) {
+			textArr = defaultText['normal'].split(' ');
+		} else {
+			textArr = defaultText['wars'].split(' ');
+		}
 
 		// setup new text words
-		for (var i = 0; i < parseInt(params[1].val); i++) {
-			newTextWords += textArr[i] + ' ';
+		for (i = 0; i < parseInt(params[1].val); i++) {
+			if (i < 69) {
+				newTextWords += textArr[i] + ' ';
+			} else {
+				if (i % 69 === 0) {
+					j = 0;
+				}
+				
+				newTextWords += textArr[j] + ' ';
+				j++;
+			}
+			
 		}
 
 		// setup new text paras
-		for (var i = 1; i <= parseInt(params[0].val); i++) {
-			newTextParas += newTextWords;
+		if (params[2].val == 0) {
 
-			console.log(i, parseInt(params[0].val), !(parseInt(params[0].val) == i));
+			// no tags
+			for (var i = 1; i <= parseInt(params[0].val); i++) {
+				newTextParas += newTextWords;
 
-			if (!(parseInt(params[0].val) == i)) {
-				 newTextParas += '&#10;&#10;';
+				if (!(parseInt(params[0].val) == i)) {
+					 newTextParas += '&#10;&#10;';
+				}
+			}
+		} else {
+
+			// with tags
+			for (var i = 1; i <= parseInt(params[0].val); i++) {
+				newTextParas += '<p>' + newTextWords + '</p>';
+
+				if (!(parseInt(params[0].val) == i)) {
+					 newTextParas += '&#10;&#10;';
+				}
 			}
 		}
+
+		//****************************/
+		// TO DO
+		// add full stop to final word...
+		//****************************/ 
 
 		// setup the new url for bookmarking
 		setUrlParams();
 
-		// finish by setting, stretching textarea & selecting it
-		textarea.innerHTML = (newTextParas);
-		textarea.style.height = textarea.scrollHeight + "px";
-		textarea.select();
-
-
-
-
-		/*********/
-		// TO DO
-		// update meta info....
-		/*********/
+		// finish by setting, stretching $textarea
+		$textarea.innerHTML = (newTextParas);
+		$textarea.style.height = 0;
+		$textarea.style.height = $textarea.scrollHeight + "px";	
 	}
 
 
 	// SET THE URL PARAMS FOR BOOKMARKING
 	function setUrlParams() {
+		//****************************/
+		// TO DO
+		// bookmarking url
+		//****************************/
+	}
+
+
+	// UPDATE USER DATA & META
+	function updateUI() {
+		var $paras = document.querySelector('.paras-input');
+		var $words = document.querySelector('.words-input');
+		var $tags = document.querySelector('.tags-input');
+		var $wars = document.querySelector('.wars-input');
 		
+		// update paras
+		$paras.value = params[0].val;
+
+		// update words
+		$words.value = params[1].val;
+
+		// update tags
+		if (params[2].val == 0) {
+			$tags.checked = false;
+		} else {
+			$tags.checked = true;
+		}
+
+		// update wars
+		if (params[3].val == 0) {
+			$wars.checked = false;
+		} else {
+			$wars.checked = true;
+		}
+
+		// update meta
+		document.querySelector('.meta-input').value = params[0].val * params[1].val;
+
+		// output lipsum text
+		generate();
 	}
 
 
@@ -76,37 +147,28 @@ var Lipsum = (function() {
 			 }
 			 params[i].val = newparam;
 		}
-		console.log(params);
 
-
-		/*********/
-		// TO DO
-		// update values for user....
-		/*********/
-
-
-		// generate text
-		generate();
+		updateUI();
+		document.querySelector('.lipsum-text').select();
 	})();
 
 
 	// FUNCTION CALLED ON GENY INTERACTION
 	Lipsum.update = function(param, value) {
+
+		// update params to new value
 		for (var i = 0; i < params.length; i++) {
 			if (params[i].name == param) {
 				params[i].val = value;
 				break;
 			}
 		}
-		// update values for user
-		//...
 
-		// generate text
-		generate();
+		updateUI();
 	}
 
 
-	// return
+	// RETURN MODULE
 	return Lipsum;
 
 })();
@@ -115,8 +177,86 @@ var Lipsum = (function() {
 // INIT
 // *****************************************************************
 (function() {
-	
-	
-	// Lipsum.getUrlParams();
+	// PARAS
+	// more
+	document.querySelector('.paras .arrow.more').addEventListener('click', function() {
+		var curvalue = parseInt(document.querySelector('.paras-input').value);
+		var newvalue = (curvalue + 1).toString();
+
+		Lipsum.update('paras', newvalue);
+	});
+	// less
+	document.querySelector('.paras .arrow.less').addEventListener('click', function() {
+		var curvalue = parseInt(document.querySelector('.paras-input').value);
+		var newvalue = (curvalue - 1).toString();
+
+		if (parseInt(newvalue) < 1) {
+ 			newvalue = '1';
+	 	}		
+
+		Lipsum.update('paras', newvalue);
+	});
+	// keyup
+	document.querySelector('.paras-input').addEventListener('keyup', function() {
+		Lipsum.update('paras', this.value);
+	});
+
+	// WORDS
+	// more
+	document.querySelector('.words .arrow.more').addEventListener('click', function() {
+		var curvalue = parseInt(document.querySelector('.words-input').value);
+		var newvalue = (curvalue + 1).toString();
+
+		Lipsum.update('words', newvalue);
+	});
+	// less
+	document.querySelector('.words .arrow.less').addEventListener('click', function() {
+		var curvalue = parseInt(document.querySelector('.words-input').value);
+		var newvalue = (curvalue - 1).toString();
+
+		if (parseInt(newvalue) < 1) {
+ 			newvalue = '1';
+	 	}		
+
+		Lipsum.update('words', newvalue);
+	});
+	// keyup
+	document.querySelector('.words-input').addEventListener('keyup', function() {
+		Lipsum.update('words', this.value);
+	});
+
+	// TAGS
+	document.querySelector('.tags-input').addEventListener('change', function() {
+
+		var newvalue;
+
+		if (this.checked) {
+			newvalue = '1';
+		} else {
+			newvalue = '0';
+		}
+
+		Lipsum.update('tags', newvalue);
+	});
+
+	// WARS
+	document.querySelector('.wars-input').addEventListener('change', function() {
+
+		var newvalue;
+
+		if (this.checked) {
+			newvalue = '1';
+		} else {
+			newvalue = '0';
+		}
+
+		Lipsum.update('wars', newvalue);
+	});
+
+	// TEXT SELECT
+	document.querySelector('.lipsum-text').addEventListener('click', function() {
+		this.select();
+	});
+
 })();
 
--- libs/src/js/script/init.js
+++ libs/src/js/script/init.js
@@ -2,8 +2,86 @@
 // INIT
 // *****************************************************************
 (function() {
-	
-	
-	// Lipsum.getUrlParams();
+	// PARAS
+	// more
+	document.querySelector('.paras .arrow.more').addEventListener('click', function() {
+		var curvalue = parseInt(document.querySelector('.paras-input').value);
+		var newvalue = (curvalue + 1).toString();
+
+		Lipsum.update('paras', newvalue);
+	});
+	// less
+	document.querySelector('.paras .arrow.less').addEventListener('click', function() {
+		var curvalue = parseInt(document.querySelector('.paras-input').value);
+		var newvalue = (curvalue - 1).toString();
+
+		if (parseInt(newvalue) < 1) {
+ 			newvalue = '1';
+	 	}		
+
+		Lipsum.update('paras', newvalue);
+	});
+	// keyup
+	document.querySelector('.paras-input').addEventListener('keyup', function() {
+		Lipsum.update('paras', this.value);
+	});
+
+	// WORDS
+	// more
+	document.querySelector('.words .arrow.more').addEventListener('click', function() {
+		var curvalue = parseInt(document.querySelector('.words-input').value);
+		var newvalue = (curvalue + 1).toString();
+
+		Lipsum.update('words', newvalue);
+	});
+	// less
+	document.querySelector('.words .arrow.less').addEventListener('click', function() {
+		var curvalue = parseInt(document.querySelector('.words-input').value);
+		var newvalue = (curvalue - 1).toString();
+
+		if (parseInt(newvalue) < 1) {
+ 			newvalue = '1';
+	 	}		
+
+		Lipsum.update('words', newvalue);
+	});
+	// keyup
+	document.querySelector('.words-input').addEventListener('keyup', function() {
+		Lipsum.update('words', this.value);
+	});
+
+	// TAGS
+	document.querySelector('.tags-input').addEventListener('change', function() {
+
+		var newvalue;
+
+		if (this.checked) {
+			newvalue = '1';
+		} else {
+			newvalue = '0';
+		}
+
+		Lipsum.update('tags', newvalue);
+	});
+
+	// WARS
+	document.querySelector('.wars-input').addEventListener('change', function() {
+
+		var newvalue;
+
+		if (this.checked) {
+			newvalue = '1';
+		} else {
+			newvalue = '0';
+		}
+
+		Lipsum.update('wars', newvalue);
+	});
+
+	// TEXT SELECT
+	document.querySelector('.lipsum-text').addEventListener('click', function() {
+		this.select();
+	});
+
 })();
 
